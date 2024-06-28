9ff0eb76d1ac5604dcd4dac6e6799cf119fe44d3
Lova <lova.sobeus@hotmail.com>
2024-06-28 16:08:05 +0200

    new aurelia project

---
+++ flashcard-app/src/main.ts
@@ -0,0 +1,27 @@
+import {Aurelia} from 'aurelia-framework';
+import environment from '../config/environment.json';
+import {PLATFORM} from 'aurelia-pal';
+import 'bootstrap/dist/css/bootstrap.css';
+
+if ((module as any).hot) {
+  (module as any).hot.accept();
+  (module as any).hot.dispose(() => {
+    const aurelia = (window as any).aurelia;
+    aurelia.container.get('EventAggregator').publish('hot-reload');
+    aurelia.setRoot(PLATFORM.moduleName('app')).then(() => console.log('Aurelia hot-reloaded'));
+  });
+}
+
+export function configure(aurelia: Aurelia): void {
+  aurelia.use
+    .standardConfiguration()
+    .feature(PLATFORM.moduleName('resources/index'));
+
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+
+  if (environment.testing) {
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+  }
+
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+}
